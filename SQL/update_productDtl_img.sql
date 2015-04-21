f26f80af2e714d8f674ffefcedbde6df4687dc0f
Triplesic <triplesic@gmail.com>
2015-04-21 21:42:24 +0700

    update product_dtl image

---
+++ SQL/update_productDtl_img.sql
@@ -0,0 +1,6 @@
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img01.jpg' where product_dtl_id = 1;
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img02.jpg' where product_dtl_id = 2;
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img03.jpg' where product_dtl_id = 3;
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img04.jpg' where product_dtl_id = 4;
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img05.jpg' where product_dtl_id = 5;
+update BELLAVITAGOWN_DB.PRODUCT_DTL set image_name = 'img06.jpg' where product_dtl_id = 6;
