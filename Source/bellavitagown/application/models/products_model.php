aeda9a6d99fcc03f9af53c6d8d91b67738eae6fa
keelato <keelatomail@gmail.com>
2015-04-29 01:18:02 +0700

    แก้query การแสดงผลหน้า productdetail

--- Source/bellavitagown/application/models/products_model.php
+++ Source/bellavitagown/application/models/products_model.php
@@ -3,9 +3,9 @@ Class Products_Model extends CI_Model
 {
  function getNewProduct()
  {
-   $this -> db -> select('p.PRODUCT_ID,p.PRODUCT_NM_TH,p.PRODUCT_NM_EN,p.PRICE,pd.IMAGE_NAME');
+   $this -> db -> select('p.PRODUCT_ID,p.PRODUCT_NM_TH,p.PRODUCT_NM_EN,p.PRICE,pd.PRODUCT_DTL_ID,pd.IMAGE_NAME');
    $this -> db -> from('product AS p');
-   $this -> db -> join('product_dtl AS pd' , 'p.PRODUCT_ID = pd.PRODUCT_ID');
+   $this -> db -> join('product_dtl AS pd' , 'p.PRODUCT_ID = pd.PRODUCT_ID', 'left');
    $this->db->group_by('pd.PRODUCT_ID');
    $this->db->order_by('pd.UPDATED_DATE', 'desc');
    
@@ -17,14 +17,29 @@ Class Products_Model extends CI_Model
 
  }
  
- function getProductDetail($product_ID)
+ function getProduct($product_ID)
  {
- 	$this -> db -> select('p.PRODUCT_NM_TH,p.PRODUCT_NM_EN,p.PRICE,pd.PRODUCT_DTL_ID,pd.IMAGE_NAME,pd.DETAIL,pd.COLOR,pd.SIZE,s.QUANTITY');
+ 	$this -> db -> select('p.PRODUCT_ID,p.PRODUCT_NM_TH,p.PRODUCT_NM_EN,p.PRICE,pd.PRODUCT_DTL_ID,pd.IMAGE_NAME,pd.DETAIL,pd.COLOR,pd.SIZE,s.QUANTITY');
  	$this -> db -> from('product AS p');
- 	$this -> db -> join('product_dtl AS pd' , 'p.PRODUCT_ID = pd.PRODUCT_ID');
- 	$this -> db -> join('stock AS s' , 's.PRODUCT_DTL_ID = pd.PRODUCT_DTL_ID');
+ 	$this -> db -> join('product_dtl AS pd' , 'p.PRODUCT_ID = pd.PRODUCT_ID', 'left');
+ 	$this -> db -> join('stock AS s' , 's.PRODUCT_DTL_ID = pd.PRODUCT_DTL_ID', 'left');
  	$this -> db -> where('p.PRODUCT_ID = ' . "'" . $product_ID . "'");	
  	$this->db->order_by('pd.PRODUCT_DTL_ID', 'desc');
+ 	$this -> db -> group_by('pd.COLOR');
+ 	$query = $this -> db -> get();
+ 
+ 	return $query->result();
+ 
+ }
+ 
+ function getProductDetail($product_DTL_ID)
+ {
+ 	$this -> db -> select('p.PRODUCT_ID,p.PRODUCT_NM_TH,p.PRODUCT_NM_EN,p.PRICE,pd.PRODUCT_DTL_ID,pd.IMAGE_NAME,pd.DETAIL,pd.COLOR,pd.SIZE,s.QUANTITY');
+ 	$this -> db -> from('product_dtl AS pd');
+ 	$this -> db -> join('product AS p' , 'p.PRODUCT_ID = pd.PRODUCT_ID', 'left');
+ 	$this -> db -> join('stock AS s' , 's.PRODUCT_DTL_ID = pd.PRODUCT_DTL_ID', 'left');
+ 	$this -> db -> where('pd.PRODUCT_DTL_ID = '  . $product_DTL_ID );
+ 	
  	$query = $this -> db -> get();
  
  	return $query->result();
