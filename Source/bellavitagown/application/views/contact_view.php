93ca272030295f77f64f8671f92ae2bbb6454f55
keelato <keelatomail@gmail.com>
2015-08-07 23:46:34 +0700

    จัดใหม่

--- Source/bellavitagown/application/views/contact_view.php
+++ Source/bellavitagown/application/views/contact_view.php
@@ -5,51 +5,20 @@
 				<h2 class="title text-center">
 					<strong><?php echo lang('contact_us')?></strong>
 				</h2>
-				<!-- 
-				  <img src="<?php echo base_url();?>images/location-01.png"
-					class="girl img-responsive" alt=""
-					style="margin-top: -5px; padding: 0; display: inline-block;" />
-				  -->
+			</div>
+		</div>
+		
+		<div class="row">		
+			<div class="col-sm-8" style="text-align: center;">
 				<div id="gmap" class="contact-map">
 					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.84429219525!2d100.40038004880043!3d13.667230343694964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDQwJzAwLjYiTiAxMDDCsDIzJzU3LjQiRQ!5e0!3m2!1sen!2s!4v1431350768484"
-						width="900" height="400" frameborder="0" style="border: 0">
+						width="700" height="400" frameborder="0" style="border: 0">
 					</iframe>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-8">
-				<div class="contact-form">
-					<h2 class="title text-center"><?php echo lang('ask_me')?></h2>
-					<div class="status alert alert-success" style="display: none"></div>
-					<form id="main-contact-form" class="contact-form row"
-						name="contact-form" method="post">
-						<div class="form-group col-md-6">
-							<input type="text" name="name" class="form-control"
-								required="required" placeholder="<?php echo lang('name')?>">
-						</div>
-						<div class="form-group col-md-6">
-							<input type="email" name="email" class="form-control"
-								required="required" placeholder="<?php echo lang('email')?>">
-						</div>
-						<div class="form-group col-md-12">
-							<input type="text" name="subject" class="form-control"
-								required="required" placeholder="<?php echo lang('subject')?>">
-						</div>
-						<div class="form-group col-md-12">
-							<textarea name="message" id="message" required="required"
-								class="form-control" rows="8" placeholder="<?php echo lang('message')?>"></textarea>
-						</div>
-						<div class="form-group col-md-12">
-							<input type="submit" name="submit"
-								class="btn btn-primary pull-right" value="<?php echo lang('submit')?>">
-						</div>
-					</form>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="contact-info">
+			<div class="col-sm-4" style="text-align: left;">
+			<div class="contact-info">
 					<h2 class="title text-center"><?php echo lang('contact_info')?></h2>
 					<address>
 						<p>Bella Vita Co.,Ltd.</p>
@@ -59,6 +28,8 @@
 						<p><?php echo lang('phone')?>: (66)2-416-0314</p>
 						<p><?php echo lang('phone')?>: (66)2-895-0013-4</p>
 						<p><?php echo lang('email')?>: bellavita_gown@hotmail.com</p>
+						<p><span>* </span>หากคุณสนใจสินค้าโปรดส่งข้อมูลของคุณ</p>
+						<p align="center"><button class="btn btn-default get" onclick="inquiryPopup();">Contact Now</button></p>
 					</address>
 					<div class="social-networks">
 						<h2 class="title text-center">Social Networking</h2>
@@ -70,8 +41,10 @@
 						</ul>
 					</div>
 				</div>
+			
 			</div>
 		</div>
+		
 	</div>
 </div>
 <!--/#contact-page-->
