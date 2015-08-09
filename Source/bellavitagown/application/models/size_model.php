95b3941096e6724bbeb755574f889b4ec1d005cf
keelato <keelatomail@gmail.com>
2015-08-09 11:12:31 +0700

    แก้ไข sql

--- Source/bellavitagown/application/models/size_model.php
+++ Source/bellavitagown/application/models/size_model.php
@@ -4,7 +4,7 @@ Class Size_Model extends CI_Model
  function getProductSize($productDTLID)
  {
  	
-   $this -> db -> select('S.SIZE_ID,s.SIZE_NM');
+   $this -> db -> select('s.SIZE_ID,s.SIZE_NM');
    $this -> db -> from('product_dtl_size AS p');
    $this -> db -> join('size AS s' , 's.SIZE_ID = p.SIZE_ID', 'left');
    $this -> db -> where('p.PRODUCT_DTL_ID = ' . $productDTLID );
